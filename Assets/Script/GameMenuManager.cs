9389b092e0bbcf0dcbea87288af34cf5b05a14e8
Resminos <loic75019@hotmail.fr>
2022-11-30 16:35:22 +0100

    Update GameMenuManager.cs

--- Assets/Script/GameMenuManager.cs
+++ Assets/Script/GameMenuManager.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class GameMenuManager : MonoBehaviour
 {
     public GameObject menu;
+
+    public void playGame()
+    {
+        //SceneManager.LoadScene(1);
+    }
+    
+    public void quitGame()
+    {
+        Application.Quit();
+    }
     
     // Start is called before the first frame update
     void Start()
