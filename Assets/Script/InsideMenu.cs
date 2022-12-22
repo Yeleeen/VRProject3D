8072a0903700732e5978adf95362fd79f5d07615
Yeleeen <yeleen.doumbia@gmail.com>
2022-12-22 19:06:02 +0100

    PauseButton working

---
+++ Assets/Script/InsideMenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class InsideMenu : MonoBehaviour
+{
+
+    public GameObject questionCanva;
+    public GameObject pauseMenuCanva;
+    public Button questionMenuButton;
+    public Button pauseMenuButton;
+    public Button resumeMenuButton;
+    public Button leaveMenuButton;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+      Debug.Log("start");
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        questionMenuButton.onClick.AddListener(HideQuestionCanvas);
+        pauseMenuButton.onClick.AddListener(HideMenuCanvas);
+
+        resumeMenuButton.onClick.AddListener(HideMenuCanvas);
+
+        leaveMenuButton.onClick.AddListener(SceneMenu);
+        
+    }
+    void HideQuestionCanvas()
+    {
+        Debug.Log("Pausequestion");
+        questionCanva.SetActive(false);
+        pauseMenuCanva.SetActive(true);
+    }
+    void HideMenuCanvas()
+    {
+        Debug.Log("Pause");
+
+        pauseMenuCanva.SetActive(false);
+        questionCanva.SetActive(true);
+    }
+
+    void SceneMenu(){
+        SceneManager.LoadScene(1); 
+    }
+}
